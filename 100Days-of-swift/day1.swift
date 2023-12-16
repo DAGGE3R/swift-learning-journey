68ef098d2a28e03fe50980dce024f2b64c0f10b3
DAGGE3R <aziz.saadana@isitc.u-sousse.tn>
2023-12-16 13:07:58 +0100

    second push 👏

--- 100Days-of-swift/day1.swift
+++ 100Days-of-swift/day1.swift
@@ -1,20 +1,33 @@
 
-let a = 21 ; // Define a constant 'a' with a value of 21
-let b = 2 ; // Define a constant 'b' with a value of 2
+let a : Int = 21 ; // Define a constant 'a' with a value of 21
+//! a = 10 Error: Cannot assign to value: 'a' is a 'let' constant
 
-print(a+b) // Print the sum of 'a' and 'b'
 
 // create an array of 5 elements
 var arr : [Int] = [1,2,3,4,5]
-
-// map the array to a new array with each element multiplied by 2
-var arr2 : [Int] = arr.map({$0 * 2})
-print(arr2) // Print the new array 'arr2' with each element multiplied by 2
+print(arr.map({$0 * 2})) // Print each element multiplied by 2
 
 
-func sum(_ num1: Int, _ num2: Int) -> Int {
-    return num1 + num2
+func sum(_ num1: Int, _ num2: Int) -> String {
+    return( "your sum is \(num1 + num2) " ) // this is string interpolation
 }
-
 print(sum(1,2)) // Print the sum of 1 and 2
 
+//tuple
+var name = (first: "Taylor", last: "Swift")
+
+print(name.first)
+
+
+func premier(_ num1 : Int) -> Bool {
+    if (num1 % 2 == 0) {
+        return false
+    }else {return true}
+}
+
+func range(_ num1 : Int) -> Void {
+    for i : Int in 0...num1 {
+      premier(i) ? print(i) : print("")
+    }
+}
+range(100)
